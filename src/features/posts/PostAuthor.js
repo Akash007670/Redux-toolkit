e1a23f05f1a56a47501b1a941a0eabc21f528ae4
Akash kumar <akaysharma894@gmail.com>
2023-03-08 00:15:23 +0530

    Added TimeAgo feature

--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -5,7 +5,7 @@ import { selectAllUsers } from "../users/usersSlice";
 const PostAuthor = ({ userId }) => {
   const users = useSelector(selectAllUsers);
   const author = users.find((user) => user.id == userId);
-  return <span>{author ? author.name : "Unknown Author"}</span>;
+  return <span>By {author ? author.name : "Unknown Author"}</span>;
 };
 
 export default PostAuthor;
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
@@ -6,16 +7,19 @@ const initialState = [
     title: "Learning React",
     description:
       "It's an Amzing Library which ive been using for the past 1.5 years",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: 2,
     title: "Learning Redux and Redux Toolkit",
     description: "Started Learing this deeply from today onwards",
+    date: sub(new Date(), { minutes: 12 }).toISOString(),
   },
   {
     id: 3,
     title: "Learning Node.js",
     description: "Started one month ago but couldn't stick to it.",
+    date: sub(new Date(), { minutes: 19 }).toISOString(),
   },
 ];
 
@@ -33,6 +37,7 @@ export const postsSlice = createSlice({
             id: nanoid(),
             title,
             description,
+            date: new Date().toISOString(),
             userId,
           },
         };
